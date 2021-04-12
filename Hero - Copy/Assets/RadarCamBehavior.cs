77ae26d320cc51c7258485d38dfa9b43d0db7ae5
RaceWilliams3 <willr108@my.erau.edu>
2021-04-11 23:22:33 -0700

    MiniMap

---
+++ Hero - Copy/Assets/RadarCamBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarCamBehavior : MonoBehaviour
+{
+    private Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, 30, target.position.z);
+        this.transform.eulerAngles = new Vector3(90, target.transform.eulerAngles.y, 0);
+    }
+}
